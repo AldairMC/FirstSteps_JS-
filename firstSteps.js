85d34e64b2df94add25d927b58e7529296378d8a
AldairMC <aldairmc2023@gmail.com>
2018-08-21 20:46:31 -0500

    Array

---
+++ firstSteps.js
@@ -0,0 +1,30 @@
+/*
+  var notaUno, notaDos, prom;
+  notaUno = 10;
+  notaDos = "5";
+  prom = notaUno + Number(notaDos);
+
+  alert(prom)
+  */
+
+  // las variables inician con var
+
+  //Number convierte string a integer
+
+  var nombre = prompt("Ingrese su nombre");
+  var apellido = prompt("Ingrese su apellido");
+
+  document.write("<h1>");
+
+  document.write(nombre + " " + apellido);
+
+  document.write("</h1>");
+
+  //prompt abre una ventana para ingresar datos
+
+
+  var articulos = ["hola", "bienvenido"];
+
+  var articulos = new Array("hola", "bienvenido");
+
+  //dos manera de hacer un Array
