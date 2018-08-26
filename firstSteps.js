ad0dd390fe3fecf6bee94b72093f16a2208f92b9
AldairMC <aldairmc2023@gmail.com>
2018-08-25 20:14:05 -0500

    Method pop and shift

--- firstSteps.js
+++ firstSteps.js
@@ -33,8 +33,16 @@
   articulos.unshift("hey"); //agregar elementos al inicio del array
 
   document.write("<p> Para agregar elementos al final utilizamos </p>");
-  articulos.push("aquí"); //agregaelementos al final del arrat
+  articulos.push("aqui"); //agregaelementos al final del arrat
 
   document.write(articulos[1]); //muestra el primer elemento del array
 
   document.write("<p>" + articulos[articulos.length - 1] + "</p>"); // muestra el ultimo elemento del array
+
+  document.write("<p> Para borrar un elemento al final utilizamos </p>");
+  articulos.pop("aqui"); //Elimina elemento de un array al final
+
+  document.write("<p> Para eliminar elementos al inicio utilizamos </p>");
+  articulos.shift("hola"); //elimina elementos al inicio de un array
+
+  document.write(articulos[articulos.length - 1]);
