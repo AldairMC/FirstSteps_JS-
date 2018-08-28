5390707088a5d77a638db03173b1e50b9ba61c91
AldairMC <aldairmc2023@gmail.com>
2018-08-27 22:11:35 -0500

    Conditional if, else if, else

--- poo.js
+++ poo.js
@@ -1,3 +1,28 @@
-var btn = document.getElementById('inicio');
+/*var btn = document.getElementById("inicio");
 
 btn.style.width="200px";
+btn.style.height="300px";
+*/
+// condicional
+
+/*
+var precioCoche = 60000;
+var dineroAhorrado = 15000;
+
+if (dineroAhorrado >= precioCoche){
+  alert("Puedes comprar el coche!! Enorabuena");
+}
+  alert("Lo siento no tienes fondos suficientes");
+*/
+
+var notaUno = 3.0;
+var notaDos = 5.0;
+var notaTres = 4.5;
+var suma = (notaUno+notaDos+notaTres)/3
+
+if (suma >= 3.0){
+  alert("Felicidades");
+}else if (suma == 2.0){
+  alert("debes esforzarte");
+}
+  alert("Lo siento");
