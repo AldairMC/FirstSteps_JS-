1c26eee4b42fd837b9ab16db62ace0420450447a
AldairMC <aldairmc2023@gmail.com>
2018-09-06 22:02:47 -0500

    Practical exercice finished

--- Exercice.js
+++ Exercice.js
@@ -1,5 +1,30 @@
 //Practical Exercice
-var nMinimo = 1;
-var nMaximo = 100;
+//número maximo
+var nMaximo = 3;
 
-var numeroSecreto*(math.randon(nMinimo-nMaximo));
+//número nMinimo
+var nMinimo = 1;
+
+//numeor secreto
+var numeroSecreto = Math.randon()*(nMaximo-nMinimo)+nMinimo;
+numeroSecreto = ParseInt(numeroSecreto); //convertir numeroa entero
+
+var msj = "Por favor ingresa el posibole número secreto"
+
+while (true) {
+  var numeroUsuario = prompt(msj, "0");
+
+  numeroUsuario = ParseInt(numeroUsuario);
+
+  if (numeroSecreto === numeroUsuario) {
+    alert("Hey loco ganaste!!!");
+    break;
+  }else if (numeroUsuario > numeroSecreto) {
+    msj = ("Hey loco el número secreto es más bajo");
+  }else if (numeroUsuario < numeroSecreto) {
+    msj = ("Hey loco el número secreto es más alto");
+  }else if (numeroUsuario === 0) {
+    break;
+  }
+
+}
