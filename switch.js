13ed79b3272de8d6026ab39f0d6c0a231ab0fc5c
AldairMC <aldairmc2023@gmail.com>
2018-09-09 20:29:13 -0500

    Switch

---
+++ switch.js
@@ -0,0 +1,14 @@
+var typecard = "Credic card";
+
+switch (typecard) {
+    case "Credic card":
+        console.log("Tienes una tarjeta de credito");
+        break;
+
+    case "Debit card":
+        console.log("Tienes una tarjeta de debito");
+        
+    default:
+    console.log("No tienes una tarjeta");
+        break;
+}
